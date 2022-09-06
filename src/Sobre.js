5a472bf1de9ece0456047908c97b8e36dfafd647
Gabriel-Yuzo <gandrade529@gmail.com>
2022-09-05 21:10:42 -0300

    COMMIT

--- src/Sobre.js
+++ src/Sobre.js
@@ -1,11 +1,15 @@
-import React from 'react';
-import { Link } from "react-router-dom";
+import React, { useDebugValue } from 'react';
+import { Link, useHistory, useLocation } from "react-router-dom";
 
 export default function Sobre(){
+   const location = useLocation()
+   const history = useHistory() 
    return (
-       <div>
+       <><div>
            <h1>Sobre o site</h1>
+           <h1>Path: {location.pathname}</h1>
+
            <Link to="/">retornar a página inicial</Link>
-       </div>
+       </div><h2> {history.action}</h2></>
    );
 }
